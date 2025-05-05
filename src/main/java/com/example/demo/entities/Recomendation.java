08a4e7d1a97ab9448565a89622e9c8ae2eb47c2a
Collax-ops <andresjuliancollazoscastro@gmail.com>
2025-05-04 21:39:21 -0500

    Configuracion INicial

---
+++ src/main/java/com/example/demo/entities/Recomendation.java
@@ -0,0 +1,32 @@
+package com.example.demo.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "recomendations")
+public class Recomendation {
+    @Id
+    @Column(name = "recomendation_id", nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "userid", nullable = false)
+    private User userid;
+
+    @Column(name = "title", length = Integer.MAX_VALUE)
+    private String title;
+
+    @Column(name = "description", length = Integer.MAX_VALUE)
+    private String description;
+
+/*
+ TODO [Reverse Engineering] create field to map the 'category' column
+ Available actions: Define target Java type | Uncomment as is | Remove column mapping
+    @Column(name = "category", columnDefinition = "recomendation_category_enum")
+    private Object category;
+*/
+}
