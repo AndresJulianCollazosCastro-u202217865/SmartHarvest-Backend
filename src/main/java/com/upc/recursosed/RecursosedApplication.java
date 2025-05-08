014ce1d67dff4560cda4eb5a4d873e07f8487fef
LuissCHG <u202216444@upc.edu.pe>
2025-05-07 22:07:36 -0500

    Prueba

---
+++ src/main/java/com/upc/recursosed/RecursosedApplication.java
@@ -0,0 +1,13 @@
+package com.upc.recursosed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecursosedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecursosedApplication.class, args);
+    }
+
+}
